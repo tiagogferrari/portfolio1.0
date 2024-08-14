bad099d13fbf130e1ac5fcb155c1c719bda2d105
Tiago Ferrari <tiagoggferrari@gmail.com>
2024-08-14 12:17:04 -0300

    estilizacao class profile

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNavbar from './components/navbar';
+import Button from 'react-bootstrap/Button';
+import gmailLogo from './assets/logos/gmail.png';
+import githubLogo from './assets/logos/github.png';
+import linkedinLogo from './assets/logos/linkedin.png';
+import whatsappLogo from './assets/logos/whatsapp.png';
+
 
 function App() {
+
+  const scrollToProjects = () => {
+    document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className="App">
       <MyNavbar />
-      <div class="profile">
-        <p id="name">TIAGO FERRARI</p>
-        <p id="function">20 anos, ADS - UTFPR</p>
-        <p id="location">Adamantina, São Paulo, Brasil</p>
+      <div className="profile">
+        <div className="profile-info">
+          <p id="name">TIAGO FERRARI</p>
+          <p id="function">20 anos, Análise De Sistemas - UTFPR</p>
+          <Button variant="dark" id="projButton" onClick={scrollToProjects}>Ver meus projetos!</Button>
+        </div>
+        <div className="profile-logos">
+          <a href="https://wa.me/5518996826368" target="_blank" rel="noopener noreferrer">
+            <img src={whatsappLogo} alt="WhatsApp" className="navbar-logo" id="logo" width={30} height={30} />
+          </a>
+          <a href="https://mail.google.com/mail/?view=cm&fs=1&to=tiagoggferrari@gmail.com&body=Olá, Tiago! Acessei seu portfólio e achei muito legal." target="_blank" rel="noopener noreferrer">
+            <img src={gmailLogo} alt="Gmail" className="navbar-logo" id="logo" width={30} height={30} />
+          </a>
+          <a href="https://www.linkedin.com/in/tiago-garcez-ferrari-783833270/" target="_blank" rel="noopener noreferrer">
+            <img src={linkedinLogo} alt="LinkedIn" className="navbar-logo" id="logo" width={30} height={30} />
+          </a>
+          <a href="https://github.com/tiagogferrari" target="_blank" rel="noopener noreferrer">
+            <img src={githubLogo} alt="GitHub" className="navbar-logo" id="logo" width={30} height={30} />
+          </a>
+        </div>
       </div>
-      <div className="projetos">
-
+      <div id="projects">
+        oi
       </div>
-
-
-
     </div>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import '../components/navbar.css';
 
-import gmailLogo from '../assets/logos/gmail.png';
-import githubLogo from '../assets/logos/github.png';
-import linkedinLogo from '../assets/logos/linkedin.png';
-import whatsappLogo from '../assets/logos/whatsapp.png';
 
 const MyNavbar = () => {
+
+    useEffect(() => {
+        function updateDateTime() {
+            const now = new Date();
+            const formattedDateTime = now.toLocaleString('pt-BR', {
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+
+            });
+
+            const dateTimeElement = document.getElementById('dateTime');
+            if (dateTimeElement) {
+                dateTimeElement.textContent = "Adamantina, São Paulo, Brasil - " + formattedDateTime;
+            }
+        }
+
+        const intervalId = setInterval(updateDateTime, 1000); // Atualiza a cada segundo
+        updateDateTime(); // Atualiza imediatamente ao carregar a página
+
+        return () => clearInterval(intervalId); // Limpa o intervalo ao desmontar o componente
+    }, []); // O array vazio garante que o efeito seja executado apenas uma vez
+
     return (
         <Navbar expand="lg" className="bg-body-tertiary" id='navbar'>
             <Container fluid id='container'>
-                <Navbar.Brand href="#home">TF</Navbar.Brand>
-                <div className="navbar-logos ms-auto">
-                    <a href="https://wa.me/5518996826368" target="_blank" rel="noopener noreferrer">
-                        <img src={whatsappLogo} alt="WhatsApp" className="navbar-logo" id="logo" width={30} height={30} />
-                    </a>
-                    <a href="https://mail.google.com/mail/?view=cm&fs=1&to=tiagoggferrari@gmail.com&body=Olá, Tiago! Acessei seu portfólio e achei muito legal." target="_blank" rel="noopener noreferrer">
-                        <img src={gmailLogo} alt="Gmail" className="navbar-logo" id="logo" width={30} height={30} />
-                    </a>
-                    <a href="https://www.linkedin.com/in/tiago-garcez-ferrari-783833270/" target="_blank" rel="noopener noreferrer">
-                        <img src={linkedinLogo} alt="LinkedIn" className="navbar-logo" id="logo" width={30} height={30} />
-                    </a>
-                    <a href="https://github.com/tiagogferrari" target="_blank" rel="noopener noreferrer">
-                        <img src={githubLogo} alt="GitHub" className="navbar-logo" id="logo" width={30} height={30} />
-                    </a>
-                </div>
+                <Navbar.Brand href="#home" id="dateTime"></Navbar.Brand>
+
             </Container>
         </Navbar>
     );
